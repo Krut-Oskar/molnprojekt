e358b63454256dfd0129aebe750623bb7359ffc7
William <whensen0@gmail.com>
2021-12-17 19:58:51 +0100

    added style to website and logic to direct to either bike or train page depending on temp

--- web/flasktest/app.py
+++ web/flasktest/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import requests
 import json
 import datetime
@@ -14,25 +14,39 @@ def index():
     func_json = response_func.json()
     for i in func_json:
         temp = i["temperature"]
+    if temp > 25:
+        return redirect(url_for("bike"))
+    else:
+        return redirect(url_for("train"))
 
+@app.route('/bike')
+def bike():
+    response_func = requests.get(os.getenv("DATA_URL"))
+    func_json = response_func.json()
+    for i in func_json:
+        temp = i["temperature"]
+    temp_corr = str.format("{0:.1f}",temp)
+    dt = datetime.datetime.now();
+    clock = dt.strftime('%H:%M:%S')
+    return render_template("bike.html", temp=temp_corr, clock=clock)
+
+@app.route('/train')
+def train():
     lista = []
     response_api = requests.get(os.getenv("API_URL"))
     api_json = response_api.json()
     for item in api_json["Departure"]:
-        lista.append(item["time"])
         lista.append(item["direction"])
-
-    return render_template('index.html', lista=lista, temp=temp)
-
-@app.route('/api')
-def get_data():
-    testList = []
-    data = requests.get(os.getenv("API_URL"))
-    json_data = data.json()
-    for item in json_data["Departure"]:
-        testList.append(item["direction"])
-        testList.append(item["time"])
-    return render_template('api.html', lista=testList)
+        lista.append(item["time"])
+    length = len(lista)
+    response_func = requests.get(os.getenv("DATA_URL"))
+    func_json = response_func.json()
+    for i in func_json:
+        temp = i["temperature"]
+    temp_corr = str.format("{0:.1f}",temp)
+    dt = datetime.datetime.now();
+    clock = dt.strftime('%H:%M:%S')
+    return render_template('train.html', lista=lista, clock=clock, temp=temp_corr, length=length)
 
 if __name__ == "__main__":
     app.run(debug=True)
