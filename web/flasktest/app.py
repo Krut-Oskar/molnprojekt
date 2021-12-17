f3d11651fcf515e80392f54c61af33fa7c912cb4
William <whensen0@gmail.com>
2021-12-17 14:10:11 +0100

    correct working with api

--- web/flasktest/app.py
+++ web/flasktest/app.py
@@ -1,17 +1,30 @@
 from flask import Flask, render_template
 import requests
 import json
+import datetime
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/asd/')
+@app.route('/api')
 def get_data():
-    r = requests.get("http://127.0.0.1:7071/api/GetCosmosDB")
-    r_json = r.json()
-    temp = r_json.get("temperature")
-    ts = r_json.get("_ts")
-    
+    testList = []
+    data = requests.get(os.getenv("API_URL"))
+    json_data = data.json()
+    for item in json_data["Departure"]:
+        testList.append(item["time"])
+        testList.append(item["direction"])
+        
+        
+    # temp = data["temperature"]
+    # ts = datetime.datetime.fromtimestamp(data["_ts"])
+    return render_template('api.html', lista=testList)
+
+if __name__ == "__main__":
+    app.run(debug=True)
